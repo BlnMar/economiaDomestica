ed6083106f38898859db2a5ab07b0d5ec5437578
alumnofp <alumnofp@FPB25.CALASANZSALAMANCA.local>
2023-10-11 12:31:26 +0200

    listar conceptos

--- JS/script.js
+++ JS/script.js
@@ -1,61 +1,88 @@
+var arrayImportes = [];
 
-var arrayGastos = [];
-
-var arrayContador = [];//contar importe e imprimir su string
+var arrayConceptos = []; //contar importe e imprimir su string
 
 var contador = 0;
 
 function gastos(importe) {
+  //vaciar el marcador al pulsar gasto nuevo mes
+  let gastototal = document.getElementById("gastototal");
+  gastototal.innerHTML = "";
 
-    //vaciar el marcador al pulsar gasto nuevo mes
-    let gastototal = document.getElementById("gastototal");
-    gastototal.innerHTML = "";
+  arrayImportes.push(importe);
+  console.log(arrayImportes);
 
-    arrayGastos.push(importe);
-    console.log(arrayGastos);
+  if (importe == 150) {
+    arrayConceptos.push("Luz y gas");
+    //añadirConcepto(); mal, imprime con cada gasto, no al final
+    contador++;
+  } else if (importe == 20) {
+    arrayConceptos.push("Agua y basura");
+    contador++;
+  } else if (importe == 40) {
+    arrayConceptos.push("Internet");
+    contador++;
+  } else if (importe == 65) {
+    arrayConceptos.push("Telefono");
+    contador++;
+  } else if (importe == 800) {
+    arrayConceptos.push("Hipoteca");
+    contador++;
+  } else if (importe == 15) {
+    arrayConceptos.push("Seguros");
+    contador++;
+  } else if (importe == 100) {
+    arrayConceptos.push("Comida");
+    contador++;
+  } else if (importe == 70) {
+    arrayConceptos.push("Gasolina");
+    contador++;
+  } else if (importe == 365) {
+    arrayConceptos.push("Colegio");
+    contador++;
+  } else if (importe == 35) {
+    arrayConceptos.push("Ocio");
+    contador++;
+  }
 
-     /*falta contador de gastos
-    if (importe = 150){
-        arrayContador.push("luzgas");
-        contador ++;
-    }
-    if (importe = 20){
-        arrayContador.push("aguabasura");
-        contador ++;
-    }
-    if (importe = 40){
-        arrayContador.push("internet");
-        contador ++;
-    }*/
-    
-    arrayContador.sort(); //ordena alfabeticamente
-    console.log(arrayContador);
+  console.log(arrayConceptos);
 }
 
+/*function añadirConcepto(params) {
+  let listaNombres = document.getElementById("listaNombres");
+  listaNombres.innerHTML = ""; // Limpiar la lista
+
+  for (let i = 0; i < arrayConceptos.length; i++) {
+    let conceptolista = document.createElement("li");
+    conceptolista.textContent = arrayConceptos[i];
+    listaNombres.appendChild(conceptolista);
+  }
+}*/
 
 function cierreMes() {
 
-    let sumaGastos = 0;
-
-    for (let i = 0; i < arrayGastos.length; i++) {
-      sumaGastos += arrayGastos[i];
+ /*   let conceptos;
+    for(let i=0;i<arrayConceptos.length;i++){
+        conceptos+= arrayConceptos[i];
     }
-     
-    console.log(sumaGastos);
 
-    let gastototal = document.getElementById("gastototal");
-    gastototal.innerHTML = "Gasto final: " + sumaGastos;
-
-
-   
-     
-
-    /*el ultimo paso es que el array quede vacio*/ 
-    arrayGastos.splice(0, arrayGastos.length);
-    console.log(arrayGastos);  
+    let listaNombres = document.getElementById("listaNombres");
+    listaNombres.innerHTML=listaNombres;*/
     
-    arrayContador.splice(0,arrayContador.length); 
-    console.log(arrayContador);
-}
 
- 
+  let sumaGastos = 0;
+  for (let i = 0; i < arrayImportes.length; i++) {
+    sumaGastos += arrayImportes[i];
+  }
+  console.log(sumaGastos);
+
+  let gastototal = document.getElementById("gastototal");
+  gastototal.innerHTML = "Gasto final: " + sumaGastos;
+
+  /*el ultimo paso es que el array quede vacio*/
+  arrayImportes.splice(0, arrayImportes.length);
+  console.log(arrayImportes);
+
+  arrayConceptos.splice(0, arrayConceptos.length);
+  console.log(arrayConceptos);
+}
